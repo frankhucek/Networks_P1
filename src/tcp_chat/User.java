8f7acf8c4faf5187b56df60f30355a25e3c2a443
Frank Hucek <fjh32@case.edu>
2016-11-08 16:23:46 -0500

    WORKING USERNAME setting. Added User class and handling server commands

---
+++ src/tcp_chat/User.java
@@ -0,0 +1,99 @@
+package tcp_chat;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ *
+ * @author frank
+ */
+public class User
+{
+    private Socket userSocket;
+    private DataOutputStream myData_OUT;
+    private BufferedReader myData_IN;
+    private String username;
+    
+    public User(Socket userSocket) throws IOException
+    {
+        this.userSocket = userSocket;
+        this.myData_OUT = new DataOutputStream(this.userSocket.getOutputStream());
+        this.myData_IN = new BufferedReader(new InputStreamReader(
+                                                userSocket.getInputStream()));
+    }
+    
+    public User(String username, Socket userSocket) throws IOException
+    {
+        this(userSocket);
+        this.username = username;
+    }
+
+    /**
+     * @return the userSocket
+     */
+    public Socket getUserSocket()
+    {
+        return userSocket;
+    }
+
+    /**
+     * @param userSocket the userSocket to set
+     */
+    public void setUserSocket(Socket userSocket)
+    {
+        this.userSocket = userSocket;
+    }
+
+    /**
+     * @return the myData_OUT
+     */
+    public DataOutputStream getmyData_OUT()
+    {
+        return myData_OUT;
+    }
+
+    /**
+     * @param myData_OUT the myData_OUT to set
+     */
+    public void setmyData_OUT(DataOutputStream myData_OUT)
+    {
+        this.myData_OUT = myData_OUT;
+    }
+
+    /**
+     * @return the myData_IN
+     */
+    public BufferedReader getmyData_IN()
+    {
+        return myData_IN;
+    }
+
+    /**
+     * @param myData_IN the myData_IN to set
+     */
+    public void setmyData_IN(BufferedReader myData_IN)
+    {
+        this.myData_IN = myData_IN;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername()
+    {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+    
+    
+}
--- src/tcp_chat/UserThread.java
+++ src/tcp_chat/UserThread.java
@@ -1,9 +1,6 @@
 package tcp_chat;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,19 +14,9 @@ public class UserThread extends Thread
     private final Socket myListeningSocket;
     private Socket talkingToSocket;
     
-    public String myUsername;
-    public boolean isChatting;
+    private ConcurrentHashMap<String, UserThread> userList; 
     
-    private ConcurrentHashMap<String, UserThread> userList;
-    
-    /*
-    IN to THIS UserThread
-    OUT to Client(s)
-    */
-    private final BufferedReader myData_IN;
-    private       BufferedReader theirData_IN;
-    private final DataOutputStream myData_OUT;
-    private       DataOutputStream theirData_OUT; 
+    private User USER1, USER2;
     
     public UserThread(Socket connSocket, 
             ConcurrentHashMap<String, UserThread> userSockets) throws IOException
@@ -38,15 +25,18 @@ public class UserThread extends Thread
         this.talkingToSocket = null;
         
         this.userList = userSockets;
-        this.myUsername = "";
-        this.isChatting = false;
         
-        this.myData_IN = new BufferedReader (
-                         new InputStreamReader (
-                                 myListeningSocket.getInputStream()));
-        
-        this.myData_OUT = new DataOutputStream(myListeningSocket.getOutputStream());
+        this.USER1 = new User(connSocket);
+        this.USER2 = null; // until connected to second user
     }
+    public UserThread(Socket connSocket, 
+            ConcurrentHashMap<String, UserThread> userSockets, 
+            User existingUser) throws IOException
+    {
+        this(connSocket,userSockets);
+        this.USER1 = existingUser;
+    }
+    
 
     @Override
     public void run()
@@ -59,7 +49,144 @@ public class UserThread extends Thread
             
             while(true)
             {
-                input = myData_IN.readLine(); // server awaits input
+                handleInput(USER1);
+                handleInput(USER2);
+            }
+        } catch (IOException ex)
+        {
+            System.out.println("ERROR sending data");
+        }
+    }
+    
+    private void initializeUsername() throws IOException
+    {
+        if(USER1.getUsername() == null)
+        {
+            System.out.println("SERVER: connected to client");
+            boolean set;
+            do
+            {
+                String input = USER1.getmyData_IN().readLine(); // FIRST MESSAGE RECEIVED
+            
+                set = setUsername(input);
+            
+                if(!set)
+                    USER1.getmyData_OUT().write(UserThread.formatOutput("Invalid Username. Please enter a new one."));
+                else
+                    USER1.getmyData_OUT().write(UserThread.formatOutput("set"));
+            } while (!set);
+        
+            System.out.println("SERVER: Client username set to " + USER1.getUsername());
+        }
+    }
+    
+    private boolean setUsername(String inputName)
+    {
+        if(userList.containsKey(inputName))
+            return false;
+        else
+        {    
+            userList.put(inputName, this);
+            USER1.setUsername(inputName);
+            return true;
+        } 
+    }
+    ////////////////// ABOVE IS SERVER - CLIENT INITIALIZATION /////////////////
+     
+    
+    private void handleInput(User user) throws IOException
+    {
+        if(user != null)
+        {
+            String userIN = user.getmyData_IN().readLine();
+            
+            if(isServerCommand(userIN))
+            {
+                handleServerCommand(user, userIN);
+            }
+            else // relay messages
+            {
+                
+            }
+        }
+    }
+    
+    /**
+     * SERVER COMMANDS DELINEATED BY '-'
+     * ALL server commands of the form "-x input"
+     * where x is the option
+     * To chat with another user: -c UserToChatWith
+     * @param input
+     * @return 
+     */
+    private static boolean isServerCommand(String input)
+    {
+        return (input != null && input.startsWith("-"));
+    }
+    
+    /**
+     * 
+     * @param input 
+     */
+    private void handleServerCommand(User user, String input) throws IOException
+    {
+        
+        if(input != null)
+        {    
+            String command = input.substring(0, 2);
+            String commandData = input.substring(3);
+            switch (command)
+            {
+                case "-c": // chat
+                    if(isChatting()) // if is in chat and want to connect with other
+                    {
+                        // move other user to its own thread
+                    }
+                    this.userChatSetup(user, commandData);
+                    
+                    break;
+                    
+                default:
+                    // some default case if not an actual command
+                    break;
+            }
+                    
+        }
+        
+        handleInput(user); // can therefore call as many commands as possible
+    }
+    
+    private void userChatSetup(User user, String userToConnect)
+    {
+        
+    }
+    
+    /**
+     * Determines whether user 1 is chatting with user 2 or no one.
+     * @return 
+     */
+    public synchronized boolean isChatting()
+    {
+        return USER2 != null;
+    }
+    
+    /**
+     * Format output to all sockets on a DataOutputStream
+     * @param outputText
+     * @return 
+     */
+    public static byte[] formatOutput(String outputText)
+    {
+        return (outputText + "\n").getBytes();
+    }
+}
+
+
+/*
+
+    logic stuff.
+
+                input = USER1.myData_IN.readLine(); // server awaits input
                 
                 // check for control message
                 
@@ -82,66 +209,5 @@ public class UserThread extends Thread
                 III. both options process control messages the same
                 
                 ^^^ Implemented above
-                */
-                break;
-            }
-            myListeningSocket.close();
-        } catch (IOException ex)
-        {
-            System.out.println("ERROR sending data");
-        }
-    }
-    
-    /*
-    read initial input to get user/state
-    typical case:   put this user in hashmap with connsocket,
-                    request user, 
-                    find user in hashmap,
-                    retrieve that user's connection socket from hashmap,
-                    this thread listens from one end and writes that to the other end (possible issue with concurrent read/writes to sockets)
-                    
-    */
-    
-    private void initializeUsername() throws IOException
-    {
-        System.out.println("SERVER: connected to client");
-        boolean set;
-        do
-        {
-            String input = myData_IN.readLine(); // FIRST MESSAGE RECEIVED
-            
-            set = setUsername(input);
-            
-            if(!set)
-                myData_OUT.write(UserThread.formatOutput("Invalid Username. Please enter a new one."));
-            else
-                myData_OUT.write(UserThread.formatOutput("set"));
-        } while (!set);
-        
-        System.out.println("SERVER: Client username set to " + myUsername);
-    }
-    
-    private boolean setUsername(String inputName)
-    {
-        if(userList.containsKey(inputName))
-            return false;
-        else
-        {    
-            userList.put(inputName, this);
-            myUsername = inputName;
-            return true;
-        } 
-    }
-    
-    
-    
-    /**
-     * Format output to all sockets on a DataOutputStream
-     * @param outputText
-     * @return 
-     */
-    public static byte[] formatOutput(String outputText)
-    {
-        return (outputText + "\n").getBytes();
-    }
-}
+                /
+                break;*/
